8ede869b0ed0fcef4219816524b9ab5501955902
root <root@LAPTOP-9KCAVK69.localdomain>
2025-07-25 15:10:59 +0530

    hello

---
+++ c_practice/test1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("enter the numbers:");
+	scanf("%d",&a);
+	printf("the value of a is:%d",a);
+	return 0;
+}
---
+++ c_practice/test3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	char ch;
+	printf("enter the character:");
+	scanf("%c",&ch);
+	printf("ASCII value of a character is:%d",ch);
+	return 0;
+}
